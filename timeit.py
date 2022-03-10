7bdb895728c28fc9947948410961bd94abd6a6b4
AnhVuong146 <99466654+AnhVuong146@users.noreply.github.com>
2022-03-09 20:44:23 -0800

    Add files via upload

---
+++ timeit.py
@@ -0,0 +1,19 @@
+import time
+
+def calculate_time(func):
+    def wrapper():
+        start = time.time()
+        temp = func()       
+        end = time.time()
+        run_time = end - start
+        output = (f"Total Time {run_time}seconds")
+        print(output)
+        return temp
+    return wrapper
+
+@calculate_time
+def t1():
+    i = 10000000
+    while i > 0:
+        i -= 1
+t1()
